2e7a6af63aeb607a52a4908235b8fc21b7f258d5
leandrowanderley <devleandromcz@gmail.com>
2025-04-28 20:36:18 -0300

    Remove unused comments in model download script

--- dowload_model.py
+++ dowload_model.py
@@ -3,7 +3,6 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 model_name = "cardiffnlp/twitter-roberta-base-sentiment"
 save_directory = "./twitter-roberta-sentiment-local"
 
-# Baixa e salva o tokenizer e o modelo
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
